c51daa402aba6c32fd4a6205f4ae1b8137dbce70
tshanli <tshanli@outlook.com>
2021-01-25 19:52:13 +0100

    update: new entry for Windows To Go

---
+++ install.sh
@@ -0,0 +1,6 @@
+fontdir="/home/jiang2ji/.fonts"
+
+./matter.py -i arch folder _ _ microsoft-windows microsoft-windows \
+-hl ef233c -fg edf2f4 -bg 2b2d42 \
+-ff $fontdir/JosefinSans-Regular.ttf \
+-fn Josefin Sans Regular -fs 32
