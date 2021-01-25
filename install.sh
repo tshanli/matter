0b536a728af409343ae090f086980c038780cabb
tshanli <tshanli@outlook.com>
2021-01-26 00:12:04 +0100

    feat: add chrome os

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 fontdir="/home/jiang2ji/.fonts"
 
-./matter.py -i arch folder _ _ microsoft-windows microsoft-windows \
+./matter.py -i arch folder _ _ microsoft-windows google-chrome \
 -hl ef233c -fg edf2f4 -bg 2b2d42 \
 -ff $fontdir/JosefinSans-Regular.ttf \
 -fn Josefin Sans Regular -fs 32
