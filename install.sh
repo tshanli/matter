1db2f8b6521edc534e646f12de5e144daf0200b6
tshanli <tshanli@outlook.com>
2021-01-26 13:45:45 +0100

    update: entries adjust

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-fontdir="/home/jiang2ji/.fonts"
+fontdir="/usr/share/fonts/TTF"
 
-./matter.py -i arch folder _ _ microsoft-windows google-chrome \
+./matter.py -i microsoft-windows google-chrome folder _ _ arch \
 -hl ef233c -fg edf2f4 -bg 2b2d42 \
--ff $fontdir/JosefinSans-Regular.ttf \
--fn Josefin Sans Regular -fs 32
+-ff "$fontdir/Roboto Mono Nerd Font Complete.ttf" \
+-fn "Roboto Mono Nerd Font Complete" -fs 32
