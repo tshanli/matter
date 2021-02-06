f7f809ac70af8f05295eebe6b0ded35a053418b3
tshanli <tshanli@outlook.com>
2021-02-06 01:08:41 +0100

    update: entries

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-fontdir="/usr/share/fonts/TTF"
+#!/usr/bin/bash
 
-./matter.py -i microsoft-windows google-chrome folder _ _ arch \
+fontdir="$(pwd)"
+
+sudo python3 ./matter.py -i microsoft-windows google-chrome linux-mint folder _ _ cog \
 -hl ef233c -fg edf2f4 -bg 2b2d42 \
 -ff "$fontdir/Roboto Mono Nerd Font Complete.ttf" \
 -fn "Roboto Mono Nerd Font Complete" -fs 32
